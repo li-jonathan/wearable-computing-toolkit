ce833a00ad54b416f98c40479fb0b36bd20e01bf
li-jonathan <li.jonathancj@gmail.com>
2020-10-21 14:39:03 -0400

    start func 2

--- app.py
+++ app.py
@@ -1,98 +1,129 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import filedialog 
-from combine_files2 import CombineFiles
+from combine_files import CombineFiles
 import os
 
 class GUI(tk.Frame):
+
 	def __init__(self, master=None):
+		"""Constructor."""
+
 		super().__init__(master)
 		self.master = master
 
 		self.func1_frame = Frame(master, bg="white")
+		self.func2_frame = Frame(master, bg="white")
 
 		self.src_dir = None
 		self.dest_dir = None
 
+		self.merged_file = None
+
 		self.build_app()
 		self.create_feature1_widgets()
-
-	def combine_files(self):
-		cf = CombineFiles(self.src_dir, self.dest_dir)
-		cf.run()
-
-		# combine files run log
-		for l in cf.log:
-			self.combine_files_log.insert(tk.END, l + "\n");
-		self.combine_files_log.config(state=DISABLED)
+		self.create_feature2_widgets()
 
 	def build_app(self):
+		"""Set window settings."""
+
 		self.master.title("Wearable Computing Toolkit")
 		self.master.config(background = "white") 
 		p1 = PhotoImage(file = 'icon.png') 
 		self.master.iconphoto(False, p1)
-		self.master.minsize(500, 300)
+		self.master.minsize(500, 600)
 
 
 	def create_feature1_widgets(self):
-		
-		# self.func1_frame.grid(row=0, column=0, padx=10, pady=10)
+		"""Widgets for combine files from source into destination."""
+
 		self.func1_frame.pack(pady=10)
 
 		### SOURCE DIRECTORY ###
 
+		label = tk.Label(self.func1_frame, bg="white", text="Merge and align data streams", font=("Arial 18 bold"))
+		label.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
+
 		# choose src dir label
-		self.src_dir_prompt = tk.Label(self.func1_frame, bg="white")
-		self.src_dir_prompt["text"] = "Choose source directory"
-		self.src_dir_prompt.grid(row=0, column=0, padx=5, pady=5, sticky=W)
+		src_dir_prompt = tk.Label(self.func1_frame, text="Choose source directory", bg="white")
+		src_dir_prompt.grid(row=1, column=0, padx=5, pady=5, sticky=W)
 
 		# browse src dir button
-		self.src_bf_btn = tk.Button(self.func1_frame)
-		self.src_bf_btn["text"] = "Browse..."
-		self.src_bf_btn["command"] = self.browse_src_dir
-		self.src_bf_btn.grid(row=0, column=1, padx=5, pady=5)
+		src_bf_btn = tk.Button(self.func1_frame, text="Browse...")
+		src_bf_btn["command"] = self.browse_src_dir
+		src_bf_btn.grid(row=1, column=1, padx=5, pady=5)
 
 		# display selected src dir
 		self.src_dir_lbl = tk.Label(self.func1_frame, bg="white")
-		self.src_dir_lbl.grid(row=0, column=2, padx=5, pady=5)
+		self.src_dir_lbl.grid(row=1, column=2, padx=5, pady=5)
 
 		### DESTINATION DIRECTORY ###
 
 		# choose dest dir label
-		self.dest_dir_prompt = tk.Label(self.func1_frame, bg="white")
-		self.dest_dir_prompt["text"] = "Choose destination directory"
-		self.dest_dir_prompt.grid(row=1, column=0, padx=5, pady=5, sticky=W)
+		dest_dir_prompt = tk.Label(self.func1_frame, text="Choose destination directory", bg="white")
+		dest_dir_prompt.grid(row=2, column=0, padx=5, pady=5, sticky=W)
 
 		# browse dest dir button
-		self.dest_bf_btn = tk.Button(self.func1_frame)
-		self.dest_bf_btn["text"] = "Browse..."
-		self.dest_bf_btn["command"] = self.browse_dest_dir
-		self.dest_bf_btn.grid(row=1, column=1, padx=5, pady=5)
+		dest_bf_btn = tk.Button(self.func1_frame, text="Browse...")
+		dest_bf_btn["command"] = self.browse_dest_dir
+		dest_bf_btn.grid(row=2, column=1, padx=5, pady=5)
 
 		# display selected dest dir
 		self.dest_dir_lbl = tk.Label(self.func1_frame, bg="white")
-		self.dest_dir_lbl.grid(row=1, column=2, padx=5, pady=5)
+		self.dest_dir_lbl.grid(row=2, column=2, padx=5, pady=5)
 
 		### COMBINE FILES ###
 
 		# combine files button
-		self.combine_files_btn = tk.Button(self.func1_frame)
-		self.combine_files_btn["text"] = "Combine files"
-		self.combine_files_btn["command"] = self.combine_files
-		self.combine_files_btn.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
+		combine_files_btn = tk.Button(self.func1_frame, text="Combine files")
+		combine_files_btn["command"] = self.combine_files
+		combine_files_btn.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
 
 		self.combine_files_log = tk.Text(self.func1_frame, bg="white", width=50, height=5)
-		self.combine_files_log.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
+		self.combine_files_log.grid(row=4, column=0, columnspan=3, padx=5, pady=5)
 
 	def browse_src_dir(self):
+		"""Browse for source directory."""
+
 		selected_dir = filedialog.askdirectory(parent=root, initialdir=os.getcwd(), title='Please select the source directory')
 		self.src_dir = selected_dir[selected_dir.rfind('/')+1:] + "/"
 		self.src_dir_lbl["text"] = self.src_dir
 
 	def browse_dest_dir(self):
+		"""Browse for destination directory."""
+
 		selected_dir = filedialog.askdirectory(parent=root, initialdir=os.getcwd(), title='Please select the destination directory')
 		self.dest_dir = selected_dir[selected_dir.rfind('/')+1:] + "/"
 		self.dest_dir_lbl["text"] = self.dest_dir
+
+	def combine_files(self):
+		"""Combine files using src and dest."""
+
+		cf = CombineFiles(self.src_dir, self.dest_dir)
+		cf.run()
+
+		for l in cf.log:
+			self.combine_files_log.insert(tk.END, l + "\n");
+		self.combine_files_log.config(state=DISABLED)
+
+
+	def create_feature2_widgets(self):
+		self.func2_frame.pack(pady=10)
+
+		label = tk.Label(self.func2_frame, bg="white", text="Label datasets with activity names", font=("Arial 18 bold"))
+		label.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
+
+		# browse merged file button
+		merged_file_bf_btn = tk.Button(self.func2_frame, text="Browse for merged file")
+		merged_file_bf_btn["command"] = self.browse_merged_file
+		merged_file_bf_btn.grid(row=1, column=1, padx=5, pady=5)
+
+	def browse_merged_file(self):
+		"""Browse for source directory."""
+
+		selected_file = filedialog.askopenfilename(parent=root, initialdir=os.getcwd(), title='Please select the merged file')
+		self.merged_file = selected_file
+		self.merged_file_lbl["text"] = self.merged_file
 
 if __name__ == '__main__':
 	root = tk.Tk()
--- combine_files.py
+++ combine_files.py
@@ -1,224 +1,123 @@
-
-'''
-This script will be used to combine all csv files from the 'DataDump" git folder into one csv
-Original Script: Amanda Redhouse 6.28.18
-
-7.2.18
-Modified to use paths to/from folders as function parameters. -JF
-
-11.25.19
-Updated program to use more pandas calls and less manual manipulation. Right to Left joins
-are performed based upon https://mbientlab.com/tutorials/Apps.html#synchronizing-multiple-datasets-in-python
-Program will now check if files all have same sample rate and will merged on shortest file time. -JF
-
-1.12.20
-Add argparse to check that source and destination are supplied
-
-TODO:
-Remove hard-coded file output name
-
-
-Demo: within the tools/ folder in the data_tools branch run the following commands:
-cp *.csv src/.  #copies the sample csv files into src/
-python combine_files --dest dest/ --src src/ #should merge files and make a new “merged.csv” in dest/
-
-
-'''
 import pandas as pd
 import os
 import sys
 import argparse
 
-#### Begin MBientDataFile Class ####
+from mbient_data_file import MBientDataFile
 
-class MBientDataFile:
+"""
+CombineFiles
+From: https://git.cs.jmu.edu/WearableComputing/ActivityRecognition/blob/data_tools/Python/tools/combine_files.py
+"""
 
-    #file path to source file
-    filePath=None
+class CombineFiles():
 
-    #sensor/data type
-    sensorType=None
+	def __init__(self, source, destination):
+		"""Constructor."""
 
-    #sampling rate for data
-    sampleRate=None
+		self.src_folder = source
+		self.dest_folder = destination
+		self.log = []
+		
+	def make_paths_absolute(self):
+		"""Math paths absolute."""
 
-    #user assigned name for sensor
-    sensorName=None
+		self.log.append("Making paths absolute...")
+		if os.path.isabs(self.src_folder) != True:
+			self.src_folder = os.path.abspath(self.src_folder)
+		if os.path.isabs(self.dest_folder) != True:
+			self.dest_folder = os.path.abspath(self.dest_folder)
 
-    #data/time in UTC format that data was captured
-    captureDateTime=None
+	def validate_folders(self):
+		"""Determine if source and destination folders are valid folders."""
 
-    #sensor MAC address
-    MAC=None
+		self.log.append("Validating folders...")
+		if os.path.isdir(self.src_folder)==False:
+			self.log.append('Source folder '+self.src_folder+' cannot be found!\nExiting...')
+			sys.exit(0)
+		if os.path.isdir(self.dest_folder)==False:
+			self.log.append('Destination folder' +self.dest_folder+' cannot be found!\nExiting...')
+			sys.exit(0)
 
-    #first UNIX timestamp for data capture
-    startTime=None
+	def main(self):
+		"""Main."""
 
-    #fixed label for time column names
-    _epoch_column_label='epoc (ms)'
+		source = self.src_folder
+		dest = self.dest_folder
 
-    def _calc_sample_rate_and_start_time(self):
-        # read the first 10 rows so we can get the sample rate
-        #TODO: make this not hard coded. Will error is file is less than numRows
-        numRows = 100
-        data = pd.read_csv(self.filePath, nrows=numRows)
-        times = data[self._epoch_column_label]
-        deltas = times.diff()
+		#get all the files out of the src/ folder
+		input_file_path = source + '/' #this line may no longer be necessary
+		files = os.listdir(input_file_path)
 
-        # average period between samples in milliseconds
-        totalPeriod = deltas.sum(skipna=True) / (numRows - 1)
+		mbient_files = list()
 
-        self.sampleRate = 1 / (totalPeriod / 1000)
-        self.startTime=times.get(0)
+		for file in files:
+			if "Accelerometer" or "Gyroscope" in file:
+					dataFile= MBientDataFile(input_file_path+file)
+					mbient_files.append(dataFile)
 
+	    #basic sanity checking. See if all have same sample right and
+	    #if they start are "roughly" the same time
 
-    def __init__(self,file_path):
-        self.filePath=file_path
-        (head,tail)=os.path.split(file_path)
-        splits = tail.split("_")
+	    #Check #0 Did we get any files?!
+		if len(mbient_files) == 0:
+			self.log.append('No files in source directory. Exiting.')
+			return
 
-        self.sensorName=splits[0]
-        self.captureDateTime=splits[1]
-        self.MAC=splits[2]
-        self.sensorType=splits[3].split(".")[0]
-
-        self._calc_sample_rate_and_start_time()
-
-    def generate_data_frame(self):
-        df = pd.read_csv(self.filePath)
-
-        #delete un-needed column
-
-        del df['timestamp (-0400)'] #delete UTC timestamp column
-
-        #TODO: check to see which column exixts and then delete it based upon time
-        #del df['timestamp (-0500)']  # delete UTC timestamp column
-
-        del df['elapsed (s)'] #delete elapse column
-
-        #rename data columns to append sensor name
-        if self.sensorType=='Gyroscope':
-            df=df.rename(columns={'x-axis (deg/s)': str(self.sensorName+"_Gx")})
-            df=df.rename(columns={'y-axis (deg/s)': str(self.sensorName + "_Gy")})
-            df=df.rename(columns={'z-axis (deg/s)': str(self.sensorName + "_Gz")})
-        elif self.sensorType=='Accelerometer':
-            df=df.rename(columns={'x-axis (g)': str(self.sensorName + "_Ax")})
-            df=df.rename(columns={'y-axis (g)': str(self.sensorName + "_Ay")})
-            df=df.rename(columns={'z-axis (g)': str(self.sensorName + "_Az")})
-
-        return df
+	    #Check #1: See if files are all the same sample right
+		initRate = mbient_files[0].sampleRate
+		for i in range(1,len(mbient_files)):
+			#TODO: come up with a better method than this. 2Hz sampling error may a lot...
+			if abs(initRate - mbient_files[i].sampleRate)>2:
+				self.log.append("Error! All files are not the same sample rate!")
+				return
 
 
+	    #Check #2: See if they all start at the "same" time
+		startTimes = list()
+		for file in mbient_files:
+			startTimes.append(file.startTime)
 
-#### End MBientDataFile Class ####
+		minimum = min(startTimes)
+		maximum = max(startTimes)
 
+		delta = abs(minimum - maximum)
 
-def main(source, dest):
+		synchronize=False
+		if delta>1000:
+			self.log.append("Files have different start times. Synchronizing....")
+			synchronize=True
 
-    #get all the files out of the src/ folder
-    input_file_path = source + '/' #this line may no longer be necessary
-    files = os.listdir(input_file_path)
+	    #generate dataframes from each file. Concat into single file and
+	    #if needed to synchronize follow https://mbientlab.com/tutorials/Apps.html#synchronizing-multiple-datasets-in-python
+	    #perform left merge based upon the "oldest" file
 
-    mbient_files = list()
+	    #find the youngest file and make it "left"
+		def sortMethod(mbientFile):
+			return mbientFile.startTime
 
-    for file in files:
-        if "Accelerometer" or "Gyroscope" in file:
-            dataFile= MBientDataFile(input_file_path+file)
-            mbient_files.append(dataFile)
+		mbient_files=sorted(mbient_files,key=sortMethod,reverse=True)
 
+		left = mbient_files[0].generate_data_frame()
 
-    #basic sanity checking. See if all have same sample right and
-    #if they start are "roughly" the same time
+		for i in range(1,len(mbient_files)):
+			right = mbient_files[i].generate_data_frame()
 
-    #Check #0 Did we get any files?!
-    if len(mbient_files) == 0:
-        print('No files in source directory. Exiting.')
-        return
+	        #merge from right to left on 'epoc' column. Nearest match with 5ms tolerance
+			left = pd.merge_asof(left,right,on='epoc (ms)',direction='nearest',tolerance=5)
 
-    #Check #1: See if files are all the same sample right
-    initRate = mbient_files[0].sampleRate
-    for i in range(1,len(mbient_files)):
-        #TODO: come up with a better method than this. 2Hz sampling error may a lot...
-        if abs(initRate - mbient_files[i].sampleRate)>2:
-            print("Error! All files are not the same sample rate!")
-            return
+	    #all files are now merged into the "left" file
+		dest_file_path = dest + '/merged.csv'
 
+	    #don't print out index
+		left.to_csv(dest_file_path,index=False)
 
-    #Check #2: See if they all start at the "same" time
-    startTimes = list()
-    for file in mbient_files:
-        startTimes.append(file.startTime)
+		self.log.append("Successfully merged files in dest/merged.csv")
 
-    minimum = min(startTimes)
-    maximum = max(startTimes)
+	def run(self):
+		"""Run functions."""
 
-    delta = abs(minimum - maximum)
-
-    synchronize=False
-    if delta>1000:
-        print("Files have different start times. Synchronizing....")
-        synchronize=True
-
-    #generate dataframes from each file. Concat into single file and
-    #if needed to synchronize follow https://mbientlab.com/tutorials/Apps.html#synchronizing-multiple-datasets-in-python
-    #perform left merge based upon the "oldest" file
-
-    #find the youngest file and make it "left"
-    def sortMethod(mbientFile):
-        return mbientFile.startTime
-
-    mbient_files=sorted(mbient_files,key=sortMethod,reverse=True)
-
-    left = mbient_files[0].generate_data_frame()
-
-    for i in range(1,len(mbient_files)):
-        right = mbient_files[i].generate_data_frame()
-
-        #merge from right to left on 'epoc' column. Nearest match with 5ms tolerance
-        left = pd.merge_asof(left,right,on='epoc (ms)',direction='nearest',tolerance=5)
-
-    #all files are now merged into the "left" file
-    dest_file_path = dest + '/merged.csv'
-
-    #don't print out index
-    left.to_csv(dest_file_path,index=False)
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Combine mbient CSV files from multiple sensors into a single file.')
-    parser.add_argument('--src', dest='src', nargs=1, action="store", help='Path to a folder where all input CSV files are located',
-                        required=True)
-    parser.add_argument('--dest', dest='dest', nargs=1, action="store",
-                        help='Path to destination folder where output.csv file will be created', required=True)
-
-    args = parser.parse_args()
-
-    #pull out parameters and arguments
-    source_folder = args.src[0]
-    dest_folder = args.dest[0]
-
-    print(sour)
-
-    #make path absolute
-    if os.path.isabs(source_folder) != True:
-        source_folder = os.path.abspath(source_folder)
-
-    #make path absolute
-    if os.path.isabs(dest_folder) != True:
-        dest_folder = os.path.abspath(dest_folder)
-
-    #determine if source and destination folders are valid folders
-    if os.path.isdir(dest_folder)==False:
-        print('Destination folder' +dest_folder+' cannot be found!')
-        print('Exiting...')
-        sys.exit(0)
-    if os.path.isdir(source_folder)==False:
-        print('Source folder '+source_folder+' cannot be found!')
-        print('Exiting...')
-        sys.exit(0)
-
-    #call main with folders
-    main(source_folder,dest_folder)
-
-    print('Done!')
+		self.make_paths_absolute()
+		self.validate_folders()
+		self.main()
+		
