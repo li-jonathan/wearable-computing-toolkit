86b9eee31550b502c594aa1919789fc5b054e3b6
li-jonathan <li.jonathancj@gmail.com>
2021-02-25 11:27:02 -0500

    first version working for labeling csv file

--- csv_testing.py
+++ csv_testing.py
@@ -6,51 +6,25 @@ def read_file2():
     data = pd.read_csv('base.csv')
 
     headers = list(data.columns.values)
-    data['Activity'] = "curl"
+    print(type(headers[0]))
+
+    test = in_range(data[headers[0]])
+
+    for t in test:
+        print(type(t))
+
+
+    data['Activity'] = ""
     data.to_csv('output.csv', index=False)
 
-def read_file():
-	
-    headers = []
+def in_range(vals):
+    acts = []
 
-    ranges = {}
+    for x in vals:
+        print(float(x))
+        acts.append(float(x))
 
-    ranges = ["0-5", "6-10", "11-15"]
-
-    csvfile = open("base.csv",'r')
-    output = open("testout.csv", 'w')
-
-    data = csv.reader(csvfile, delimiter=',')
-    writer = csv.writer(output, lineterminator='\n')
-
-    for hdr in next(data):
-        print(hdr.strip())
-        headers.append(hdr.strip())
-
-    a = []
-
-    # for row in data:
-    #     print(row[0])
-
-    #     # for i in range(len(headers)):
-    #     #     print(str(i) + row[i])
-    #     print("end row")
-    #     row.append("butt")
-    #     a.append(row)
-    
-    # writer.writerows(a)
-        
-
-def in_range(key, x):
-    """key like '1601655830786-1601655830795' """
-    splitted = key.split("-")
-    start = int(splitted[0])
-    end = int(splitted[1])
-
-    if x >= start and x <= end:
-        print("in range: " + key)
-    else:
-        print("not in range")
+    return acts
 
 def main():
     read_file2()
--- label_datasets.py
+++ label_datasets.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import os
 import csv
 
@@ -50,7 +51,7 @@ class LabelDatasets:
 
 		self.root.title("Label Datasets")
 		self.root.config(background = "white") 
-		self.root.minsize(1200, 650)
+		self.root.minsize(1200, 700)
 
 	def create_gui(self):
 		"""Create widgets."""
@@ -66,12 +67,12 @@ class LabelDatasets:
 		self.activities_list['values'] = self.activities
 		self.activities_list.grid(row=0, column=1, padx=5, pady=5)
 
-		self.activities_frame.grid(row=0, column=0, padx=5, pady=5)
+		self.activities_frame.grid(row=0, column=0)
 
 		### RANGE EDITING/CONFIRMING ###
 
 		# current selected range(s) for selected activity
-		self.current_range_selections = tk.Text(self.range_edits_frame, bg="white", width=60, height=5)
+		self.current_range_selections = scrolledtext.ScrolledText(self.range_edits_frame, wrap = tk.WORD, bg="white", width=60, height=7)
 		self.current_range_selections.grid(row=0, column=0, rowspan=3, padx=5, pady=5)
 
 		# choose index of which range to delete
@@ -88,7 +89,7 @@ class LabelDatasets:
 		confirm_range_btn["command"] = self.confirm_range
 		confirm_range_btn.grid(row=2, column=1, padx=5, pady=5)
 
-		self.range_edits_frame.grid(row=1, column=0, padx=5, pady=5)
+		self.range_edits_frame.grid(row=1, column=0)
 
 		# SELECTED RANGES
 
@@ -149,7 +150,7 @@ class LabelDatasets:
 
 			self.update_ranges()
 
-		self.span = SpanSelector(ax1, onselect=onselect_func, direction='horizontal', useblit=True, span_stays=True, rectprops=dict(alpha=0.5, facecolor='red'))
+		self.span = SpanSelector(ax1, onselect=onselect_func, direction='horizontal', useblit=True, rectprops=dict(alpha=0.5, facecolor='red'))
 
 	def update_ranges(self):
 		"""Update the current range selections text box."""
@@ -196,11 +197,46 @@ class LabelDatasets:
 				self.all_ranges.insert(tk.END, entry + "\n")
 	
 	def label_file(self):
-		# label csv file with activities
+		"""Label csv file with associated activity"""
+
+		for k,v in self.activity_ranges.items():
+			print("activity=" + str(k))
+			for i in range(0, len(v)):
+				print(v[i])
+
+		data = pd.read_csv(self.merged_filename)
+		output_filename = self.merged_filename.replace(".csv", "-output.csv")
+		headers = list(data.columns.values)
+
+		activities_to_write = self.assign_activities(data[headers[0]])
+
+		data['Activity'] = activities_to_write
+		data.to_csv(output_filename, index=False)
+		
 		self.root.quit()
 		self.root.destroy()
 
+	def assign_activities(self, times):
+		acts = []
+		for t in times:
+			acts.append(self.activity_in_range(t))
+		return acts
+
+
+	def activity_in_range(self, x):
+		"""Returns the activity with a range that the given value falls into."""
 		
+		act = "Unknown"
+
+		for k,v in self.activity_ranges.items():
+			for i in range(0, len(v)):
+				start = float(v[i][0])
+				end = float(v[i][1])
+				if float(x) >= start and float(x) <= end:
+					act = k
+		
+		return act
+
 	
 	def read_file(self):
 		"""Reads file to find headers and values."""
